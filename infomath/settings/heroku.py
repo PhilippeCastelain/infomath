d93ecd6547bf1aba97d40749c23afe51652d10b8
PhilippeCastelain <76850401+PhilippeCastelain@users.noreply.github.com>
2021-02-12 18:28:52 +1100

    change settings

--- infomath/settings/heroku.py
+++ infomath/settings/heroku.py
@@ -25,3 +25,14 @@ DATABASES = {
     # read os.environ['DATABASE_URL'] and raises ImproperlyConfigured exception if not found
     'default': env.db(),
 }
+# Static files settings
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
